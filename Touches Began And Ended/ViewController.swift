975bc2e0d90477575844ac787aeba296e3d0da4b
bilal-sagir <sagir.bilal@icloud.com>
2021-11-05 21:48:34 +0300

    touchmoved

--- Touches Began And Ended/ViewController.swift
+++ Touches Began And Ended/ViewController.swift
@@ -3,11 +3,19 @@ import UIKit
 class ViewController: UIViewController
 {
 
-    override func viewDidLoad()
+    @IBOutlet weak var pink: UIView!
+    
+    override func touchesMoved(_ touches: Set<UITouch>,
+                               with event: UIEvent?)
     {
-        super.viewDidLoad()
+        if touches.count > 1 {return}
+        
+        let touch = touches.first
+        
+        if touch?.view != pink {return}
+        
+        pink.center = touch?.location(in: self.view) ?? self.view.center
     }
-
-
+    
 }
 
