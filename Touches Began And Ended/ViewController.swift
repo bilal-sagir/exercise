002998aa041409623777589cf63bb8b5da8bad7b
bilal-sagir <sagir.bilal@icloud.com>
2021-11-06 10:24:54 +0300

    gestures

--- Touches Began And Ended/ViewController.swift
+++ Touches Began And Ended/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  Touches Began And Ended
-//
-//  Created by Bilal on 5.11.2021.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
