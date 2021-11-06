fff4fbe5f5fbb910dd117a977c43bb05f5d46c50
bilal-sagir <sagir.bilal@icloud.com>
2021-11-06 10:19:34 +0300

    try to move obj without .center

--- Touches Began And Ended/ViewController.swift
+++ Touches Began And Ended/ViewController.swift
@@ -5,6 +5,16 @@ class ViewController: UIViewController
 
     @IBOutlet weak var pink: UIView!
     
+    var posOfTouch:CGPoint?
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        let touch1 = touches.first
+        print(touch1?.location(in: self.view) ?? self.view.center)
+        posOfTouch = touch1?.location(in: self.view) ?? self.view.center
+    }
+    
+    
     override func touchesMoved(_ touches: Set<UITouch>,
                                with event: UIEvent?)
     {
@@ -14,8 +24,15 @@ class ViewController: UIViewController
         
         if touch?.view != pink {return}
         
-        pink.center = touch?.location(in: self.view) ?? self.view.center
+        var fra:CGRect = pink.frame
+        
+        fra.origin.x = pink.frame.origin.x + posOfTouch!.x
+        fra.origin.y = pink.frame.origin.y + posOfTouch!.y
+        pink.frame = fra
+        
+
     }
+    
     
 }
 
