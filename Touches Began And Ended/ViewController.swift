908015974fdf24416d62bdc9e8b25cee0d8418e5
bilal-sagir <sagir.bilal@icloud.com>
2021-11-06 11:04:18 +0300

    LongPressRecognizer

--- Touches Began And Ended/ViewController.swift
+++ Touches Began And Ended/ViewController.swift
@@ -2,12 +2,23 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
+    @IBOutlet weak var red: UIView!
+    
+    
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        let longPress = UILongPressGestureRecognizer(target: self,
+                                                     action: #selector(selectorA))
+        red.addGestureRecognizer(longPress)
     }
-
+    
+    @objc
+    func selectorA ()
+    {
+        print("Long Pressed")
+    }
 
 }
 
