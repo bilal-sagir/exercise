94ed38dddc69bc28fce216d815b4e0aa9cfab974
bilal-sagir <sagir.bilal@icloud.com>
2021-11-06 10:49:50 +0300

    velocity added

--- Touches Began And Ended/ViewController.swift
+++ Touches Began And Ended/ViewController.swift
@@ -6,9 +6,12 @@ class ViewController: UIViewController
     @IBAction func rotationGesture(_ sender: UIRotationGestureRecognizer)
     {
         let rotationRadians = sender.rotation
-        print("rotation radiance = \(rotationRadians)")
+        //print("rotation radiance = \(rotationRadians)")
         let rotationInDegree = rotationRadians * 180 / CGFloat.pi
-        print("rotation in degrees = \(rotationInDegree)")
+        //print("rotation in degrees = \(rotationInDegree)")
+        
+        let rotationVelo = sender.velocity
+        print("velocity = \(rotationVelo)")
     }
 }
 
