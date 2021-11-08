e5d666f7c1d8b97adcdacb83c21f0a2daf4bdaeb
bilal-sagir <sagir.bilal@icloud.com>
2021-11-09 01:26:36 +0300

    transition UIView

--- Touches Began And Ended/ViewController.swift
+++ Touches Began And Ended/ViewController.swift
@@ -2,12 +2,22 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
-    override func viewDidLoad()
+    @IBOutlet weak var red : UIView!
+    
+    override func touchesEnded(_ touches: Set<UITouch>,
+                               with event: UIEvent?)
     {
-        super.viewDidLoad()
-    }
+        UIView.transition(with: self.red,
+                          duration: 1.5,
+                          options: UIView.AnimationOptions.transitionFlipFromTop)
+        {
+            self.red.backgroundColor = UIColor.black
+        } completion: { (res) in
+            print("done")
+            self.red.backgroundColor = UIColor.red
+        }
 
+    }
 
 }
 
