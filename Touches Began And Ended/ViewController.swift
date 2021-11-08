f38e84946cf258d7a298b8ddcf7e0722199d3c66
bilal-sagir <sagir.bilal@icloud.com>
2021-11-08 19:23:18 +0300

    cgafineTransform usage

--- Touches Began And Ended/ViewController.swift
+++ Touches Began And Ended/ViewController.swift
@@ -2,12 +2,24 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
-    override func viewDidLoad()
+    @IBOutlet weak var pink: UIView!
+    
+    
+    override func touchesEnded(_ touches: Set<UITouch>,
+                               with event: UIEvent?)
     {
-        super.viewDidLoad()
+        //pink.transform = pink.transform.translatedBy(x: 10.0, y: 20.0)
+        
+        //pink.transform = pink.transform.rotated(by: 90.0 * CGFloat.pi / 180.0)
+        
+        //reset transformation
+        pink.transform = CGAffineTransform.identity
+        
+        let rot = CGAffineTransform(rotationAngle: CGFloat.pi / 2.0)
+        let scl = CGAffineTransform(scaleX: 2, y: 2)
+        
+        pink.transform = rot.concatenating(scl)
     }
-
-
+    
 }
 
