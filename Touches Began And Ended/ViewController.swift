758a7e2a53cd7854ec3231d976b635c2b3bf9d9e
bilal-sagir <sagir.bilal@icloud.com>
2021-11-09 01:13:22 +0300

    cornerRadius and customView

--- Touches Began And Ended/ViewController.swift
+++ Touches Began And Ended/ViewController.swift
@@ -1,13 +1,39 @@
 import UIKit
 
-class ViewController: UIViewController
+@IBDesignable class CustomView: UIView
 {
-
-    override func viewDidLoad()
+    override func prepareForInterfaceBuilder()
     {
-        super.viewDidLoad()
+        self.backgroundColor = UIColor.gray
+        self.layer.borderColor = UIColor.black.cgColor
+   //     self.layer.borderWidth = 2.0
     }
+    
+    @IBInspectable var borderWidth : CGFloat = 2.0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius : CGFloat = 10.0 {
+        didSet {
+            //self.layer.cornerRadius = cornerRadius
+            let pati = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: UIRectCorner.bottomLeft, cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
+            let maskLayer = CAShapeLayer()
+            maskLayer.path = pati.cgPath
+            self.layer.mask = maskLayer
+            
+        }
+    }
+    
+}
 
 
+
+
+
+
+
+class ViewController: UIViewController{
 }
 
