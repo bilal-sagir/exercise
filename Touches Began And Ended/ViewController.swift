78804517be950b3ba98863fd3b9c45660af9c2cf
bilal-sagir <sagir.bilal@icloud.com>
2021-11-08 19:55:26 +0300

    Create scale and rotation gestures

--- Touches Began And Ended/ViewController.swift
+++ Touches Began And Ended/ViewController.swift
@@ -3,9 +3,30 @@ import UIKit
 class ViewController: UIViewController
 {
 
+    @IBOutlet weak var red: UIView!
+    
     override func viewDidLoad()
     {
-        super.viewDidLoad()
+        let rotGest = UIRotationGestureRecognizer(target: self, action: #selector(rotFunc))
+        
+        let sclGest = UIPinchGestureRecognizer(target: self, action: #selector(sclFunc))
+        
+        red.addGestureRecognizer(rotGest)
+        red.addGestureRecognizer(sclGest)
+    }
+    
+    @objc func rotFunc (_ sender: UIRotationGestureRecognizer)
+    {
+        let rotAmount = sender.rotation
+        red.transform = red.transform.rotated(by: rotAmount)
+        sender.rotation = 0.0
+    }
+    
+    @objc func sclFunc (_ billy: UIPinchGestureRecognizer)
+    {
+        let sclAmount = billy.scale
+        red.transform = red.transform.scaledBy(x: sclAmount, y: sclAmount)
+        billy.scale = 1.0
     }
 
 
