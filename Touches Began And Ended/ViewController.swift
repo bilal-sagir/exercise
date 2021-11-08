58b47b981a3e9ac115a827388c01fbf920e89b33
bilal-sagir <sagir.bilal@icloud.com>
2021-11-08 20:08:34 +0300

    create an animation

--- Touches Began And Ended/ViewController.swift
+++ Touches Began And Ended/ViewController.swift
@@ -2,12 +2,25 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
-    override func viewDidLoad()
+    @IBOutlet weak var red: UIView!
+    
+    override func touchesEnded(_ touches: Set<UITouch>,
+                               with event: UIEvent?)
     {
-        super.viewDidLoad()
+        UIView.animate(withDuration: 1.0)
+        {
+            self.red.alpha = 0.0
+            self.red.center = CGPoint(x: self.red.center.x + 100,
+                                      y: self.red.center.y)
+            
+        } completion: { (res) in
+            UIView.animate(withDuration: 1.0)
+            {
+                self.red.alpha = 1.0
+                self.red.center = CGPoint(x: self.red.center.x - 100,
+                                          y: self.red.center.y)
+            }
+        }
     }
-
-
 }
 
